8dbab0f40ab35d277d3fca9fd48da722b236eb77
TheNoeTrevino <noetrevino.music@gmail.com>
2025-09-28 07:12:37 -0700

    add: chordal hold definitions

--- keyboards/silakka54/keymaps/thenoetrevino/keymap.c
+++ keyboards/silakka54/keymaps/thenoetrevino/keymap.c
@@ -3,6 +3,15 @@
 
 #include QMK_KEYBOARD_H
 
+const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
+    LAYOUT(
+        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
+        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
+        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
+        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
+                       'L', 'L', 'L',  'R', 'R', 'R'
+    );
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // default
