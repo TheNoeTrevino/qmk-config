03765202756a07ad84e97ed8f60973d50f5aa22f
TheNoeTrevino <noetrevino.music@gmail.com>
2025-09-28 08:39:03 -0700

    tweak settings

--- keyboards/silakka54/keymaps/thenoetrevino/config.h
+++ keyboards/silakka54/keymaps/thenoetrevino/config.h
@@ -1,9 +1,9 @@
 #define TAPPING_TERM 200
-#define QUICK_TAP_TERM 90
+#define QUICK_TAP_TERM 100
 
 #define HOLD_ON_OTHER_KEY_PRESS
 #define CHORDAL_HOLD
-#define FLOW_TAP_TERM 125
+#define FLOW_TAP_TERM 105
 
 // need to mouse to moove a little slower when we tao it.
 #define MOUSEKEY_MAX_SPEED 8
