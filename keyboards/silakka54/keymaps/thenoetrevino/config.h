f005145c6b0e3786d6a7b13cf5f4d24f2d888822
TheNoeTrevino <noetrevino.music@gmail.com>
2025-09-28 07:58:52 -0700

    f

--- keyboards/silakka54/keymaps/thenoetrevino/config.h
+++ keyboards/silakka54/keymaps/thenoetrevino/config.h
@@ -1,6 +1,12 @@
 #define TAPPING_TERM 200
 #define QUICK_TAP_TERM 90
 
-#define PERMISSIVE_HOLD
+#define HOLD_ON_OTHER_KEY_PRESS
 #define CHORDAL_HOLD
-// https://docs.qmk.fm/tap_hold
+#define FLOW_TAP_TERM 125
+
+// need to mouse to moove a little slower when we tao it.
+#define MOUSEKEY_MAX_SPEED 8
+#define MOUSEKEY_DELAY 8
+#define MOUSEKEY_TIME_TO_MAX 20
+
