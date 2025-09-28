592476a990a35c64478a2b6093aa31c2057c5f75
TheNoeTrevino <noetrevino.music@gmail.com>
2025-09-28 08:15:05 -0700

    refactor KN_TRANS -> ______
    - This is just for readability. They do the same thing

--- keyboards/silakka54/keymaps/thenoetrevino/keymap.c
+++ keyboards/silakka54/keymaps/thenoetrevino/keymap.c
@@ -16,52 +16,50 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // default
     [0] = LAYOUT(
-        // TO(5),        KC_1,         KC_2,           KC_3,           KC_4,     KC_5,                               KC_6,          KC_7,        KC_8,           KC_9,            KC_0,     LT(4, KC_DEL),
               KC_PSCR,        KC_1,         KC_2,           KC_3,           KC_4,     KC_5,                               KC_6,          KC_7,        KC_8,            KC_9,            KC_0,            LT(4, KC_DEL),
                KC_TAB,        KC_Q,         KC_W,           KC_E,           KC_R,     KC_T,                               KC_Y,          KC_U,        KC_I,            KC_O,            KC_P,                  KC_BSLS,
                KC_ESC,        KC_A,   LT(3, KC_S),    LT(2, KC_D),    LT(1, KC_F),    KC_G,                               KC_H,          KC_J,        KC_K,            KC_L,            KC_SCLN,               KC_QUOT,
               KC_LSFT,  GUI_T(KC_Z), LSFT_T(KC_X),    ALT_T(KC_C),    CTL_T(KC_V),    KC_B,                               KC_N,    CTL_T(KC_M), ALT_T(KC_COMM), LSFT_T(KC_DOT),         GUI_T(KC_SLSH),        KC_RSFT,
-                                                                         CW_TOGG,  MO(4), KC_BSPC,                   KC_SPC,  KC_ENT,  KC_RSFT
-        // TODO: implement function layer
+                                                                         CW_TOGG,  MO(4), KC_BSPC,                   KC_SPC,  KC_ENT,  MO(5)
     ),
     // keypad
     [1] = LAYOUT(
-        KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_PPLS, KC_7,   KC_8,   KC_9,   KC_RBRC, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_EQL,  KC_4,   KC_5,   KC_6,   KC_RGHT, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_PMNS, KC_1,   KC_2,   KC_3,   KC_RCBR, KC_TRNS,
-                                             KC_TRNS, KC_TRNS, KC_TRNS,          KC_P0,   KC_TRNS, KC_PDOT
+        _______,  _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+        _______,  _______, _______, _______, _______, _______,                            KC_PPLS, KC_7,   KC_8,   KC_9,   KC_RBRC, _______,
+        _______,  _______, _______, _______, _______, _______,                            KC_EQL,  KC_4,   KC_5,   KC_6,   KC_RGHT, _______,
+        _______,  _______, _______, _______, _______, _______,                            KC_PMNS, KC_1,   KC_2,   KC_3,   KC_RCBR, _______,
+                                             _______, _______, _______,          KC_P0,   _______, KC_PDOT // add comma here
     ),
     // pairs
     [2] = LAYOUT(
-        KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_PPLS, KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_EQL,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_PMNS, KC_LCBR, KC_LT,   KC_GT,   KC_RCBR, KC_TRNS,
-                                             KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS
+        _______,  _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+        _______,  _______, _______, _______, _______, _______,                            _______, KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, _______,
+        _______,  _______, _______, _______, _______, _______,                            _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
+        _______,  _______, _______, _______, _______, _______,                            _______, KC_LCBR, KC_LT,   KC_GT,   KC_RCBR, _______,
+                                             _______, _______, _______,         _______,  _______,  _______
     ),
     // symbols
     [3] = LAYOUT(
-        KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_PPLS, KC_AMPR, KC_ASTR, KC_UNDS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_EQL,  KC_DLR,  KC_PERC, KC_CIRC, KC_GRV,  KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_PMNS, KC_EXLM, KC_AT,   KC_HASH, KC_TRNS, KC_TRNS,// TODO: add tilde here
-                                             KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS,  KC_TRNS, KC_PDOT
+        _______,  _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+        _______,  _______, _______, _______, _______, _______,                            KC_PPLS, KC_AMPR, KC_ASTR, KC_UNDS, _______, _______,
+        _______,  _______, _______, _______, _______, _______,                            KC_EQL,  KC_DLR,  KC_PERC, KC_CIRC, KC_GRV,  _______,
+        _______,  _______, _______, _______, _______, _______,                            KC_PMNS, KC_EXLM, KC_AT,   KC_HASH, KC_TILD, _______,
+                                             _______, _______, _______,         _______,  _______, _______
     ),
     // mouse layer
     [4] = LAYOUT(
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,        KC_TRNS,                                  KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,        KC_TRNS,                                  KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,        KC_TRNS,                                  KC_TRNS, MS_LEFT,    MS_DOWN, MS_UP,   MS_RGHT, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,        KC_TRNS,                                  KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                                 KC_TRNS,        KC_TRNS, KC_TRNS,               MS_BTN1,  MS_BTN2, KC_PDOT
+        _______,  _______, _______, _______,     _______,        _______,                        _______,  _______,    _______, _______, _______, _______,
+        _______,  _______, _______, _______,     _______,        _______,                        _______,  _______,    _______, _______, _______, _______,
+        _______,  _______, _______, _______,     _______,        _______,                        _______,  MS_LEFT,    MS_DOWN, MS_UP,   MS_RGHT, _______,
+        _______,  _______, _______, _______,     _______,        _______,                        _______,  _______,    _______, _______, _______, _______,
+                                                 _______,        _______, _______,      MS_BTN1,  MS_BTN2, KC_PDOT
     ),
     // function keys
     [5] = LAYOUT(
-        KC_TRNS,  KC_F1,   KC_F2,    KC_F3,     KC_F4,          KC_F5,                                    KC_F6,   KC_F7,      KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        KC_TRNS,  KC_F6,   KC_F7,    KC_F8,     KC_F9,          KC_F10,                                   KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_F11,  KC_F12,   KC_F13,    KC_F14,         KC_F15,                                   KC_TRNS, MS_LEFT,    MS_DOWN, MS_UP,   MS_RGHT, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS,        KC_VOLD,                                  KC_TRNS, KC_KB_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        _______,  KC_F1,   KC_F2,    KC_F3,     KC_F4,          KC_F5,                                    _______, _______,    _______, _______, _______, _______,
+        _______,  KC_F6,   KC_F7,    KC_F8,     KC_F9,          KC_F10,                                   _______, _______,    _______, _______, _______, _______,
+        _______,  KC_F11,  KC_F12,   KC_F13,    KC_F14,         KC_F15,                                   _______, MS_LEFT,    MS_DOWN, MS_UP,   MS_RGHT, _______,
+        _______,  _______, _______,  _______,   KC_VOLU,        KC_VOLD,                                  _______, KC_KB_MUTE, _______, _______, _______, _______,
                                                 KC_MPRV,        KC_MNXT, KC_MEDIA_PLAY_PAUSE,   MS_BTN1,  MS_BTN2, KC_PDOT
     ),
     // gaming layer
